a3565b21c1458dd5a11aaba650a87de41c1071ba
zengdingxing <misaki123zdx>
2019-03-05 17:19:10 +0800

    发现服务中心 eureka

---
+++ src/main/java/com/example/cloud_eureka_server/CloudEurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.example.cloud_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Miskai
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaServerApplication.class, args);
+    }
+
+}
